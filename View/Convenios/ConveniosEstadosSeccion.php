8486cb4305bea7feb10e093c900a23fbe9442738
Dibier Marin P <marindibier7@gmail.com>
2023-06-06 10:58:43 -0500

    Cambio de tabla a secciones detalle/completar estados

--- View/Convenios/ConveniosEstadosSeccion.php
+++ View/Convenios/ConveniosEstadosSeccion.php
@@ -1,9 +1,28 @@
 <h2>ESTADOS SOLICITUD</h2>
 
-    <table id="tableIntD" class="tableIntT sombra tableIntTa">
-        <tr>
-            <th>NOMBRE</th>
-            <th>DESCRIPCIÓN</th>
-            <th>ADJUNTO</th>
-        </tr>
-    </table>
+<div class="carga_Documento">
+<div class="nuevaseccion" >
+    <fieldset>        
+        <section>
+            <h6>SECOP II</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Completado</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Autorizado Coordinación</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Estado Solicitud</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+    </fieldset>
+</div>
+</div>
