2aa2bd399127a3fc966b72032b2c0102d0706294
Dibier Marin P <marindibier7@gmail.com>
2023-06-06 14:44:39 -0500

    Cambio de tabla a secciones detalle/completar estudios previos

--- View/Convenios/ConveniosEstudiosPreviosSeccion.php
+++ View/Convenios/ConveniosEstudiosPreviosSeccion.php
@@ -1,9 +1,123 @@
 <h2>ESTUDIOS PREVIOS</h2>
 
-    <table id="tableIntD" class="tableIntT sombra tableIntTa">
-        <tr>
-            <th>NOMBRE</th>
-            <th>DESCRIPCIÓN</th>
-            <th>ADJUNTO</th>
-        </tr>
-    </table>
+<div class="carga_Documento">
+<div class="nuevaseccion" >
+    <fieldset>        
+        <section>
+            <h6>Descripción de la Necesidad</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Justificación</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Análisis de Conveniencia</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Maduración del Proyecto</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Objeto</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Alcance del Objeto</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Especificaciones Técnicas</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Análisis del Sector</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Valor Total Aportes</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Disponibilidad Presupuestal</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Modalidad de Selección</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Criterios de Selección</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Análisis de Riesgo</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Garantías</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Limitaciones Mypimes</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Plazo de Ejecución</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Lugar de Ejecución</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Obligaciones de las Partes</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Forma de Pago</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Control y Vigilancia del Contrato</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Acuerdos Comerciales</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Otros Aspectos</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+        <section>
+            <h6>Conceptos Técnicos</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
+            <img src="img/icon/pdf.png" class="zoom" width=70" height=70"/>
+        </section>
+    </fieldset>
+</div>
+</div>
