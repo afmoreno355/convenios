40fd9e3f71594ecc2e6494caba0efc7c19f9a079
Dibier Marin P <marindibier7@gmail.com>
2023-06-20 11:19:42 -0500

    Llave foránea id solicitud en tabla de documentos

--- archivos/sql/convenios.sql
+++ archivos/sql/convenios.sql
@@ -1,6 +1,6 @@
 -- DROP SCHEMA public;
 
-CREATE SCHEMA public AUTHORIZATION pg_database_owner;
+CREATE SCHEMA public AUTHORIZATION postgres;
 
 COMMENT ON SCHEMA public IS 'standard public schema';
 
@@ -21,7 +21,38 @@ CREATE SEQUENCE public.solicitudes_id_solicitud_seq
 	MAXVALUE 2147483647
 	START 1
 	CACHE 1
-	NO CYCLE;-- public.documentaciones definition
+	NO CYCLE;
+-- DROP SEQUENCE public.solicitudes_id_solicitud_seq1;
+
+CREATE SEQUENCE public.solicitudes_id_solicitud_seq1
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;-- public.solicitudes definition
+
+-- Drop table
+
+-- DROP TABLE public.solicitudes;
+
+CREATE TABLE public.solicitudes (
+	id_solicitud serial4 NOT NULL,
+	nombre varchar NULL,
+	codigo_area int4 NULL,
+	mes_publicacion varchar NULL,
+	estado varchar NULL,
+	abogado varchar NULL,
+	tecnico_experto varchar NULL,
+	objeto text NULL,
+	alcance text NULL,
+	justificacion text NULL,
+	fecha_sistema date NULL,
+	CONSTRAINT solicitudes_pk PRIMARY KEY (id_solicitud)
+);
+
+
+-- public.documentaciones definition
 
 -- Drop table
 
@@ -40,28 +71,6 @@ CREATE TABLE public.documentaciones (
 	certificado_disponibilidad_presupuestal varchar NULL,
 	certificado_paa varchar NULL,
 	proyecto_autorizacion varchar NULL,
-	fecha_sistema date NULL
-);
-
-
--- public.solicitudes definition
-
--- Drop table
-
--- DROP TABLE public.solicitudes;
-
-CREATE TABLE public.solicitudes (
-	id_solicitud serial4 NOT NULL,
-	codigo_area int4 NULL,
-	abogado varchar NULL,
-	tecnico_experto varchar NULL,
-	objeto text NULL,
-	alcance text NULL,
-	especificaciones_tecnicas text NULL,
 	fecha_sistema date NULL,
-	estado varchar NULL,
-	justificacion text NULL,
-	nombre varchar NULL,
-	mes_publicacion varchar NULL,
-	CONSTRAINT solicitudes_pk PRIMARY KEY (id_solicitud)
+	CONSTRAINT documentaciones_fk FOREIGN KEY (id_solicitud) REFERENCES public.solicitudes(id_solicitud)
 );
