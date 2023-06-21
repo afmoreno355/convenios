e91fffcb2ca6b49f74b74f40ed29940aa4bfe17e
Dibier Marin P <marindibier7@gmail.com>
2023-06-20 23:07:03 -0500

    Campos de carga de documentos

--- View/Convenios/ConveniosDocumentosSeccionCompletar.php
+++ View/Convenios/ConveniosDocumentosSeccionCompletar.php
@@ -1,66 +1,78 @@
 <h2>DOCUMENTOS SOLICITUD</h2>
 <div class="carga_Documento">
 <div class="nuevaseccion" >
-    <fieldset>        
-        <section> 
-            <h6>Memorando</h6>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Estudios Previos</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Anexo Técnico</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Análisis del Sector</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70/>
-        </section>
-        <section>
-            <h6>Anexo Técnico</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70/>
-        </section>
-        <section>
-            <h6>Concepto Técnico</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Anexo Técnico</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Propuesta Técnica Económica</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70/>
-        </section>
-        <section>
-            <h6>Matriz de Riesgos</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70/>
-        </section>
-        <section>
-            <h6>Disponibilidad Presupuestal</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Plan Anual de Adqusiciones</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70 />
-        </section>
-        <section>
-            <h6>Proyecto de Autorización</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis laboriosam nisi sint corporis excepturi minus iure maxime quia sed? Vel perspiciatis ut eveniet nesciunt pariatur explicabo impedit quis commodi.</p>
-            <img src="img/icon/pdf.png" class="zoom" width=70 height=70/>
-        </section>
+    <fieldset>
+        
+        <div>
+            <fieldset>
+                <legend title='MEMORANDO'>MEMORANDO</legend>
+                <input type='file'  value=''  name='memorando' id='memorando' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='ESTUDIOS PREVIOS'>ESTUDIOS PREVIOS</legend>
+                <input type='file'  value=''  name='estudiosPrevios' id='estudiosPrevios' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='ANEXO TÉCNICO'>ANEXO TÉCNICO</legend>
+                <input type='file'  value=''  name='anexoTecnico' id='anexoTecnico' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='ANÁLISIS DEL SECTOR'>ANÁLISIS DEL SECTOR</legend>
+                <input type='file'  value=''  name='analisisSector' id='analisisSector' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='CONCEPTO TÉCNICO'>CONCEPTO TÉCNICO</legend>
+                <input type='file'  value=''  name='conceptoTecnico' id='conceptoTecnico' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='PROPUESTA TÉCNICA ECONÓMICA'>PROPUESTA TÉCNICA ECONÓMICA</legend>
+                <input type='file'  value=''  name='propuestaTecnicaEconomica' id='propuestaTecnicaEconomica' >
+            </fieldset>
+        </div>
+
+        
+        <div>
+            <fieldset>
+                <legend title='MATRIZ DE RIESGOS'>MATRIZ DE RIESGOS</legend>
+                <input type='file'  value=''  name='matrizRiesgos' id='matrizRiesgos' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='DISPONIBILIDAD PRESUPUESTAL'>DISPONIBILIAD PRESUPUESTAL</legend>
+                <input type='file'  value=''  name='disponibilidadPresupuestal' id='disponibilidadPresupuestal' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='CERTIFICACIÓN PAA'>CERTIFICACIÓN PAA</legend>
+                <input type='file'  value=''  name='paa' id='paa' >
+            </fieldset>
+        </div>
+
+        <div>
+            <fieldset>
+                <legend title='PROYECTO DE AUTORIZACIÓN'>PROYECTO DE AUTORIZACIÓN</legend>
+                <input type='file'  value=''  name='proyectoAutorizacion' id='proyectoAutorizacion' >
+            </fieldset>
+        </div>
     </fieldset>
 </div>
 </div>
--- View/Convenios/ConveniosModales.php
+++ View/Convenios/ConveniosModales.php
@@ -120,13 +120,25 @@ if ($id == 2 && $permisos)
 {
 ?>
 <!--Modal 2-->
+
+<div class="carga_Documento">
+    <div class="contenido">
+        <div class="where_title where_modal tamanio" style="width: 100%; height: auto; margin-left: 0px;">
+            <img src="img/icon/gestionar.png"/>
+            <label class="where">Convenios DFP – Dirección de Formación Profesional</label></div>
+            <br><br> 
+            <label style="font-size: 1em; " id="aviso" class="aviso" ></label> 
+            <label style="font-size: 1em; " id="aviso2" class="aviso" ><?= $convenio->getId() ?></label> 
+        </div>
+    </div>
 <?php
-    require_once(__DIR__.'/ConveniosSolicitudSeccionCompletar.php');
-    require_once(__DIR__.'/ConveniosDocumentosSeccionCompletar.php');
-    require_once(__DIR__.'/ConveniosEstudiosPreviosSeccionCompletar.php');
-    require_once(__DIR__.'/ConveniosPaaSeccionCompletar.php');
-    require_once(__DIR__.'/ConveniosEstadosSeccionCompletar.php');
+require_once(__DIR__.'/ConveniosSolicitudSeccionCompletar.php');
+require_once(__DIR__.'/ConveniosDocumentosSeccionCompletar.php');
+require_once(__DIR__.'/ConveniosEstudiosPreviosSeccionCompletar.php');
+require_once(__DIR__.'/ConveniosPaaSeccionCompletar.php');
+require_once(__DIR__.'/ConveniosEstadosSeccionCompletar.php');
 ?>
+</div>
 <?PHP
 }
 elseif ($id == 3 && $permisos)
@@ -165,11 +177,11 @@ elseif ($id == 4 && $permisos ) {
         </div>
     </div>
 <?php
-    require_once(__DIR__.'/ConveniosSolicitudSeccionMostrar.php');
-    require_once(__DIR__.'/ConveniosDocumentosSeccionMostrar.php');
-    require_once(__DIR__.'/ConveniosEstudiosPreviosSeccionMostrar.php');
-    require_once(__DIR__.'/ConveniosPaaSeccionMostrar.php');
-    require_once(__DIR__.'/ConveniosEstadosSeccionMostrar.php');
+require_once(__DIR__.'/ConveniosSolicitudSeccionMostrar.php');
+require_once(__DIR__.'/ConveniosDocumentosSeccionMostrar.php');
+require_once(__DIR__.'/ConveniosEstudiosPreviosSeccionMostrar.php');
+require_once(__DIR__.'/ConveniosPaaSeccionMostrar.php');
+require_once(__DIR__.'/ConveniosEstadosSeccionMostrar.php');
 ?>
 </div>
 <?PHP
@@ -227,7 +239,8 @@ elseif ($id == 5 && $permisos ) {
          <div style="width: auto">
             <fieldset>
                 <legend title='PASO A PASO GENERAL '>PASO A PASO GENERAL CENTROS VIRTUAL</legend>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/VCtFmKXgWks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>            </fieldset>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/VCtFmKXgWks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </fieldset>
          </div>
     </div>
 <?PHP
