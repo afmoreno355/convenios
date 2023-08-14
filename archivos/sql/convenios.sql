0a24c2abbb65d2c61a0cdc3d6f37f0d0b2d2e883
Dibier Marin P <marindibier7@gmail.com>
2023-08-14 15:33:03 -0500

    Tabla estudios previos sql

--- archivos/sql/convenios.sql
+++ archivos/sql/convenios.sql
@@ -13,6 +13,27 @@ CREATE SEQUENCE public.documentaciones_id_documentacion_seq
 	START 1
 	CACHE 1
 	NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE public.documentaciones_id_documentacion_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.documentaciones_id_documentacion_seq TO postgres;
+
+-- DROP SEQUENCE public.estudios_previos_id_estudios_previos_seq;
+
+CREATE SEQUENCE public.estudios_previos_id_estudios_previos_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE public.estudios_previos_id_estudios_previos_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.estudios_previos_id_estudios_previos_seq TO postgres;
+
 -- DROP SEQUENCE public.solicitudes_id_solicitud_seq;
 
 CREATE SEQUENCE public.solicitudes_id_solicitud_seq
@@ -22,6 +43,12 @@ CREATE SEQUENCE public.solicitudes_id_solicitud_seq
 	START 1
 	CACHE 1
 	NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE public.solicitudes_id_solicitud_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.solicitudes_id_solicitud_seq TO postgres;
+
 -- DROP SEQUENCE public.solicitudes_id_solicitud_seq1;
 
 CREATE SEQUENCE public.solicitudes_id_solicitud_seq1
@@ -30,7 +57,13 @@ CREATE SEQUENCE public.solicitudes_id_solicitud_seq1
 	MAXVALUE 2147483647
 	START 1
 	CACHE 1
-	NO CYCLE;-- public.solicitudes definition
+	NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE public.solicitudes_id_solicitud_seq1 OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.solicitudes_id_solicitud_seq1 TO postgres;
+-- public.solicitudes definition
 
 -- Drop table
 
@@ -50,6 +83,11 @@ CREATE TABLE public.solicitudes (
 	fecha_sistema date NULL,
 	CONSTRAINT solicitudes_pk PRIMARY KEY (id_solicitud)
 );
+
+-- Permissions
+
+ALTER TABLE public.solicitudes OWNER TO postgres;
+GRANT ALL ON TABLE public.solicitudes TO postgres;
 
 
 -- public.documentaciones definition
@@ -74,3 +112,58 @@ CREATE TABLE public.documentaciones (
 	fecha_sistema date NULL,
 	CONSTRAINT documentaciones_fk FOREIGN KEY (id_solicitud) REFERENCES public.solicitudes(id_solicitud)
 );
+
+-- Permissions
+
+ALTER TABLE public.documentaciones OWNER TO postgres;
+GRANT ALL ON TABLE public.documentaciones TO postgres;
+
+
+-- public.estudios_previos definition
+
+-- Drop table
+
+-- DROP TABLE public.estudios_previos;
+
+CREATE TABLE public.estudios_previos (
+	id_estudios_previos serial4 NOT NULL,
+	id_solicitud int4 NULL,
+	identificacion_dependencia_requirente text NULL,
+	descripcion_necesidad text NULL,
+	analisis_conveniencia text NULL,
+	maduracion_proyecto text NULL,
+	especificaciones_tecnicas_objeto text NULL,
+	analisis_sector text NULL,
+	valor_total_aportes text NULL,
+	desembolsos text NULL,
+	disponibilidad_presupuestal_vigencias_futuras text NULL,
+	modalidad_seleccion text NULL,
+	criterios_seleccion_objetiva text NULL,
+	analisis_riesgo text NULL,
+	garantias text NULL,
+	limitacion_mipymes text NULL,
+	plazo_ejecucion text NULL,
+	lugar_ejecucion text NULL,
+	obligaciones_partes text NULL,
+	forma_pago text NULL,
+	control_vigilancia_contrato text NULL,
+	acuerdos_comerciales text NULL,
+	otros_aspectos text NULL,
+	conceptos_tecnicos text NULL,
+	fecha_sistema date NULL,
+	CONSTRAINT estudios_previos_pk PRIMARY KEY (id_estudios_previos),
+	CONSTRAINT estudios_previos_fk FOREIGN KEY (id_solicitud) REFERENCES public.solicitudes(id_solicitud)
+);
+
+-- Permissions
+
+ALTER TABLE public.estudios_previos OWNER TO postgres;
+GRANT ALL ON TABLE public.estudios_previos TO postgres;
+
+
+
+
+-- Permissions
+
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
